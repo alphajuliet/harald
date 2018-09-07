f1460a4655954f9ebb866ac77dcc0f6fe0dffc6d
alphajuliet <andrew@alphajuliet.com>
2018-09-07 16:56:47 +1000

    Add function to deal multiple cards to a hand. Define the main state of a game. Minor code rearrangements.

--- cards.rkt
+++ cards.rkt
@@ -26,26 +26,25 @@
 (define-type Card (U 'Blk 'War 'Brd 'Sea 'Mer 'Sct))
 (define card-types '(Blk War Brd Sea Mer Sct))
 
-; A hand is a collection of zero or more cards for each character
+; A hand is a collection of zero or more cards for each character.
+; Examples of hands are: the council, each player's village, each player's hand, and the
+; cards available for refreshing the hand.
 (define-type Hand (Immutable-HashTable Card Integer))
 
-; Test data
-(define *council : Hand (hash 'Blk 1 'War 2 'Brd 3 'Sea 0 'Mer 0 'Sct 0))
-(define *hand-1  : Hand (hash 'Blk 1 'War 2 'Sea 2))
-(define *hand-2  : Hand (hash 'War 2 'Brd 1 'Mer 1))
+(define enpty-hand hash)
 
 ;-----------------------
 ; Score a player against a reference hand; by default, the council
-#;(: score (-> Hand Hand Integer))
-(define (score [player : Hand] [ref : Hand *council]) : Integer
+(: score (->* (Hand) (Hand) Integer))
+(define (score player [ref *council])
   (hash-sum
    (hash-mul player ref)))
 
 ; Show player scores
 (: scores (-> (Listof Integer)))
 (define (scores)
-    (list (score *hand-1 *council)
-          (score *hand-2 *council)))
+  (list (score *village-1 *council)
+        (score *village-2 *council)))
 
 ;-----------------------
 ; Utility function
@@ -60,12 +59,35 @@
 (define (deal-card)
   (hash (random-element card-types) 1))
 
-; Add a new card to an existing hand
-(: deal-to-hand (-> Hand Hand))
-(define (deal-to-hand h)
-  (hash-add (deal-card) h))
+; Deal n random cards to a given hand
+(: deal-n-cards (-> Integer Hand Hand))
+(define (deal-n-cards n init-h)
+  (for/fold: : Hand ([h : Hand init-h]) ([i (range 0 n)])
+    (hash-add (deal-card) h)))
 
 ;-----------------------
+; For a 2-player game, the game state is captured by the following hands:
+; - Council -> 'Council
+; - Player 1's village -> 'Village-1
+; - Player 1's hand cards -> 'Hand-1
+; - Player 2's village -> 'Village-2
+; - Player 2's hand cards -> 'Hand-2
+
+; Define the hands active in a game
+(define-type Game (Immutable-HashTable Symbol Hand))
+
+; Test data
+(define *council   : Hand (hash 'Blk 1 'War 2 'Brd 3 'Sea 0 'Mer 0 'Sct 0))
+(define *village-1 : Hand (hash 'Blk 1 'War 2 'Sea 2))
+(define *village-2 : Hand (hash 'War 2 'Brd 1 'Mer 1))
+
+; e.g.
+(: initial-state Game)
+(define initial-state (hash 'Council *council
+                            'Village-1 *village-1
+                            'Village-2 *village-2))
+
+
 ;(define-language harald)
 
 ;========================
